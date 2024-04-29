9d708f123121bf1ea96c00672aecdb534e93ba36
cveta <cvetacapova@yahoo.com>
2024-04-29 23:56:23 +0200

    post implemented and validation added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FolderUploadComponent } from './folder-upload/folder-upload.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { NavbarComponent } from './navbar/navbar.component';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {HttpClientModule} from "@angular/common/http";
+import {ToastrModule} from "ngx-toastr";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -19,6 +22,10 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
     FormsModule
   ],
   providers: [],
--- frontend/src/app/folder-upload/folder-upload.component.ts
+++ frontend/src/app/folder-upload/folder-upload.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {DataService} from "../services/data.service";
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-folder-upload',
@@ -8,16 +10,43 @@ import { Component } from '@angular/core';
 export class FolderUploadComponent {
 
   selectedFolder: any;
+  isFolderValid = false;
+
+  constructor(private uploadService: DataService, private toastrService: ToastrService) { }
 
   onFolderSelected(event: Event): void {
     const target = event.target as HTMLInputElement;
-    this.selectedFolder = (target.files as FileList)[0];
+    const file: File = (target.files as FileList)[0];
+
+    if (file) {
+      if (file.type !== 'application/zip') {
+        console.error('Invalid file type. Please upload a zip file.');
+        this.toastrService.error('Invalid file type. Please upload a zip file.');
+        return;
+      }
+
+      if (file.size > 1e9) { // 1e9 bytes = 1GB
+        console.error('File is too large. Please upload a file under 1GB.');
+        return;
+      }
+      this.isFolderValid = true;
+      this.selectedFolder = file;
+    }
   }
 
   onUpload(): void {
     if (this.selectedFolder) {
       // Here, you can implement the folder upload logic
       console.log('Folder uploaded:', this.selectedFolder);
+      this.uploadService.uploadFile(this.selectedFolder).subscribe({
+        next: (res) => {
+          console.log('File uploaded successfully:', res);
+        },
+        error: (err) => {
+          console.error('Error uploading file:', err);
+        }
+      });
+
     } else {
       console.log('No folder selected');
     }
