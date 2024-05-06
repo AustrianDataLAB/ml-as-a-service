8ce4df9ba02d5cbc2d395f565bb20e8440374805
cveta <cvetacapova@yahoo.com>
2024-05-07 00:47:20 +0200

    serving and training service

--- frontend/src/app/folder-upload/folder-upload.component.ts
+++ frontend/src/app/folder-upload/folder-upload.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {DataService} from "../services/data.service";
-import { ToastrService } from 'ngx-toastr';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-folder-upload',
@@ -11,8 +11,11 @@ export class FolderUploadComponent {
 
   selectedFolder: any;
   isFolderValid = false;
+  isUploadSuccessfull = false;
+  isServingStarted = false;
 
-  constructor(private uploadService: DataService, private toastrService: ToastrService) { }
+  constructor(private uploadService: DataService, private toastrService: ToastrService) {
+  }
 
   onFolderSelected(event: Event): void {
     const target = event.target as HTMLInputElement;
@@ -39,9 +42,12 @@ export class FolderUploadComponent {
       console.log('Folder uploaded:', this.selectedFolder);
       this.uploadService.uploadFile(this.selectedFolder).subscribe({
         next: (res) => {
+          this.toastrService.success('Folder uploaded successfully.');
           console.log('File uploaded successfully:', res);
+          this.isUploadSuccessfull = true;
         },
         error: (err) => {
+          this.toastrService.error('Error uploading file. Please try again.');
           console.error('Error uploading file:', err);
         }
       });
@@ -50,4 +56,53 @@ export class FolderUploadComponent {
       console.log('No folder selected');
     }
   }
+
+  startTraining(): void {
+    if (this.isUploadSuccessfull) {
+      console.log('Start training');
+      this.uploadService.startTraining().subscribe({
+        next: (res) => {
+          this.toastrService.success('Started training successfully.');
+        },
+        error: (err) => {
+          this.toastrService.error('Error starting training. Please try again.');
+          console.error('Error uploading file:', err);
+        }
+      });
+
+    }
+  }
+
+  startServing(): void {
+    console.log('Start serving');
+    this.uploadService.startServing().subscribe({
+      next: (res) => {
+        this.toastrService.success('Started serving successfully.');
+        this.isServingStarted = true;
+      },
+      error: (err) => {
+        this.toastrService.error('Error starting serving. Please try again.');
+        console.error('Error starting serving:', err);
+      }
+    });
+
+  }
+
+  stopServing(): void {
+    console.log('Stop serving');
+    if (this.isServingStarted) {
+      this.uploadService.stopServing().subscribe({
+        next: (res) => {
+          this.toastrService.success('Stopped serving successfully.');
+          this.isServingStarted = false;
+        },
+        error: (err) => {
+          this.toastrService.error('Error stopping serving. Please try again.');
+          console.error('Error stopping serving:', err);
+        }
+      });
+    }
+  }
+
+
 }
--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -3,20 +3,35 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class DataService {
 
-    private global_url = environment.API_URL;
-    private data_url = this.global_url + '/data'
+  private global_url = environment.API_URL;
+  private data_url = this.global_url + '/data'
+  private training_url = this.global_url + '/training'
+  private serving_url = this.global_url + '/serving'
 
-    constructor(private http: HttpClient) {
-    }
 
-    uploadFile(file: File) {
-        console.log(this.data_url)
-        const formData = new FormData();
-        formData.append('file', file);
-        return this.http.post(this.data_url, formData);
-    }
+  constructor(private http: HttpClient) {
+  }
+
+  uploadFile(file: File) {
+    console.log(this.data_url)
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post(this.data_url, formData);
+  }
+
+  startTraining() {
+    return this.http.post(this.training_url, {});
+  }
+
+  startServing() {
+    return this.http.post(this.serving_url, {});
+  }
+
+  stopServing() {
+    return this.http.delete(this.serving_url);
+  }
 }
