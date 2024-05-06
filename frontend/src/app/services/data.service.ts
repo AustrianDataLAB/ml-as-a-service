8e74e97482d39074fc3d22eaac0a85d6854dcfc8
cveta <cvetacapova@yahoo.com>
2024-05-06 17:10:31 +0200

    change env var

---
+++ frontend/src/app/services/data.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataService {
+
+    private global_url = environment.apiUrl;
+    private data_url = this.global_url + 'data'
+
+    constructor(private http: HttpClient) {
+    }
+
+    uploadFile(file: File) {
+        const formData = new FormData();
+        formData.append('file', file);
+        return this.http.post(this.data_url, formData);
+    }
+}
