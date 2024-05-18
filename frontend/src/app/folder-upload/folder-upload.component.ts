4708ee00fa3cb6dfaeafa6c858f0af67a5528a45
cveta <cvetacapova@yahoo.com>
2024-05-18 12:09:50 +0200

    ulpoad image for classification

--- frontend/src/app/folder-upload/folder-upload.component.ts
+++ frontend/src/app/folder-upload/folder-upload.component.ts
@@ -13,6 +13,10 @@ export class FolderUploadComponent {
   isFolderValid = false;
   isUploadSuccessfull = false;
   isServingStarted = false;
+  imageUrl: string | ArrayBuffer | null = null;
+  isImageValid = false;
+  labelVisible = false;
+  classificationLabel = 'Ubuntu animal';
 
   constructor(private uploadService: DataService, private toastrService: ToastrService) {
   }
@@ -37,9 +41,56 @@ export class FolderUploadComponent {
     }
   }
 
+  onImageSelected(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const file: File = (target.files as FileList)[0];
+
+    if (file) {
+      if (file.type !== 'image/jpeg'  && file.type !== 'image/png') {
+        console.error('Invalid file type. Please upload a jpeg file.');
+        this.toastrService.error('Invalid file type. Please upload a jpeg file.');
+        return;
+      }
+
+      if (file.size > 1e6) { // 1e6 bytes = 1MB
+        console.error('File is too large. Please upload a file under 1MB.');
+        this.toastrService.error('File is too large. Please upload a file under 1MB.');
+        return;
+      }
+      this.isImageValid = true;
+      this.labelVisible = true;
+
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+      };
+    }
+  }
+
   onUpload(): void {
     if (this.selectedFolder) {
       console.log('Folder uploaded:', this.selectedFolder);
+      this.uploadService.uploadFile(this.selectedFolder).subscribe({
+        next: (res) => {
+          this.toastrService.success('Folder uploaded successfully.');
+          console.log('File uploaded successfully:', res);
+          this.isUploadSuccessfull = true;
+        },
+        error: (err) => {
+          this.toastrService.error('Error uploading file. Please try again.');
+          console.error('Error uploading file:', err);
+        }
+      });
+
+    } else {
+      console.log('No folder selected');
+    }
+  }
+
+  classify(): void {
+    if (this.isImageValid) {
+      //ToDo: change to be with calssify image request
       this.uploadService.uploadFile(this.selectedFolder).subscribe({
         next: (res) => {
           this.toastrService.success('Folder uploaded successfully.');
